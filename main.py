90ddb577b19716cf571d9646ee15dc9b5bb92a02
Flarenzy <>
2025-09-01 18:27:31 +0200

    Use argparse for passing the prompt and enabling verbose

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import argparse
 import os
-import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--verbose",
+                    action="store_true",
+                    help="Print verbose output if flag is present")
+# positional arg
+parser.add_argument("prompt",
+                    action="store",
+                    type=str,
+                    default="",
+                    help="Prompt to pass to the Gemini LLM")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,16 +23,33 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     print("No api key")
     raise SystemExit(1)
-
-if len(sys.argv) < 2:
-    print("Promtp for LLM not provided")
+if args.prompt == "":
+    print("No prompt provided")
     raise SystemExit(1)
 
 client = genai.Client(api_key=api_key)
-prompt = sys.argv[1]
+user_prompt = args.prompt
 
-content_resp = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+content_resp = client.models.generate_content(model="gemini-2.0-flash-001",
+                                              contents=user_prompt)
+
+if content_resp.text is None:
+    print("Error getting resp text")
+    raise SystemExit(1)
+
 print(content_resp.text)
 
-print(f"Prompt tokens: {content_resp.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {content_resp.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {user_prompt}")
+    if content_resp.usage_metadata is None:
+        print("Error getting usage data")
+    else:
+        prompt_tokens = (f"Prompt tokens: "
+                         f"{content_resp.usage_metadata.prompt_token_count}")
+        resp_tokens = (f"Response tokens: "
+                       f"{content_resp.usage_metadata.candidates_token_count}")
+        print(prompt_tokens)
+        print(resp_tokens)
