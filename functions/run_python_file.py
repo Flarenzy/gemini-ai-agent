0e06c914967355f64f4e7d651e2af0259b45e05d
Flarenzy <>
2025-09-20 15:26:59 +0200

    run python files function

---
+++ functions/run_python_file.py
@@ -0,0 +1,57 @@
+import contextlib
+import os
+import signal
+import subprocess
+
+from functions._filepath_helpers import _is_path_allowed
+
+
+class TimeoutException(Exception):
+    pass
+
+
+def _timeout_handler(signum: int, frame):
+    raise TimeoutException("Operation timeout")
+
+
+@contextlib.contextmanager
+def timeout(seconds: int):
+    old_hanlder = signal.getsignal(signalnum=signal.SIGALRM)
+    signal.signal(signal.SIGALRM, _timeout_handler)
+    signal.alarm(seconds)
+
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+        signal.signal(signal.SIGALRM, old_hanlder)
+
+
+def run_python_file(working_directory: str,
+                    file_path: str,
+                    args: list[str] | None = None) -> str:
+    try:
+        if not _is_path_allowed(file_path, working_directory):
+            return (f'Error: Cannot execute "{file_path}" as it '
+                    'is outside the permitted working directory')
+        full_path = os.path.join(working_directory, file_path)
+        if not os.path.exists(full_path):
+            return f'Error: File "{file_path}" not found.'
+        if not full_path.endswith(".py"):
+            return f"Error: '{file_path}' is not a Python file."
+        cmd = ["python", file_path]
+        if args is not None:
+            cmd += args
+        with timeout(30):
+            result = subprocess.run(cmd,
+                                    capture_output=True,
+                                    text=True,
+                                    check=True,
+                                    cwd=working_directory)
+            if result.returncode != 0:
+                return f"Process exited with code {result.returncode}"
+            if not result.stdout:
+                return "No output produced"
+            return f"STDOUT:\n{result.stdout}\nSTDERR:\n{result.stderr}"
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ import pytest
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 EXPECTED_FOR_CURRENT = """
 Result for current directory:
@@ -127,13 +128,8 @@ def test_write_file(
 
 
 if __name__ == "__main__":
-    res = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print(res)
-    res = write_file("calculator",
-                     "pkg/morelorem.txt",
-                     "lorem ipsum dolor sit amet")
-    print(res)
-    res = write_file("calculator",
-                     "/tmp/temp.txt",
-                     "this should not be allowed")
-    print(res)
+    print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "main.py", ["3 + 5"]))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
